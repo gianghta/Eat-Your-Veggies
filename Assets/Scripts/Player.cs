b729ba4d7a6b08e1692028991f5d2e3dcfacd7d3
Giang Ta <tagh@ucmail.uc.edu>
2019-05-29 14:27:23 -0400

    Re-design character model and animations

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Storing player's key input
+        float input = Input.GetAxisRaw("Horizontal");
+
+        //Speed of the character movement
+        rb.velocity = new Vector2(input * speed, rb.velocity.y);
+    }
+}
