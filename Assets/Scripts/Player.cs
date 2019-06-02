c448d721adca6cbb70976cd610c7dfa8b7687854
Giang Ta <thoanggiang98@gmail.com>
2019-06-02 00:27:51 -0400

    Create new character model, idle/running animations and functionality

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,19 +6,44 @@ public class Player : MonoBehaviour
 {
     public float speed;
 
+    Animator anim;
     Rigidbody2D rb;
+    float input;
 
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    void Update()
+    {
+        //Set running animation when player presses keys
+        if(input != 0)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+
+        //Set player facing correct direction when running
+        if(input > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        else if(input < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+    }
+
     void FixedUpdate()
     {
         //Storing player's key input
-        float input = Input.GetAxisRaw("Horizontal");
+        input = Input.GetAxisRaw("Horizontal");
 
         //Speed of the character movement
         rb.velocity = new Vector2(input * speed, rb.velocity.y);
